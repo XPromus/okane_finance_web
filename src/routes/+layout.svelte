e25f5ee8d340b61177272ad0a7aaa3bd923a77f1
Christopher Hilgner <hilgi45@gmail.com>
2025-02-07 00:06:04 +0100

    Added skeleton to the application.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,42 @@
 <script lang="ts">
+  	import { AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
 	import '../app.css';
 	let { children } = $props();
+	let currentTile: number = $state(0);
 </script>
 
-{@render children()}
+<div class="w-screen h-screen flex flex-col">
+	<div class="bg-slate-400 p-2">
+		<span class="h3">Okane</span>
+	</div>
+	<div class="flex flex-row w-full h-full">
+		<div>
+			<AppRail>
+				<svelte:fragment slot="lead">
+					<AppRailAnchor href="/" >(icon)</AppRailAnchor>
+				</svelte:fragment>
+				<!-- --- -->
+				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 1</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 2</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 3</span>
+				</AppRailTile>
+				<!-- --- -->
+				<svelte:fragment slot="trail">
+					<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+				</svelte:fragment>
+			</AppRail>
+		</div>
+		<div>
+			{@render children()}
+		</div>
+	</div>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-
+    let count = $state(0)
 </script>
 
 <div>
     <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>    
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>   
+    <button type="button" class="btn variant-filled" onclick={() => count++}>
+        clicks: {count}
+    </button>
 </div>
