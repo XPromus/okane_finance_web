0690d70945ddabe56ce386100e8c470641eb1f49
Christopher Hilgner <hilgi45@gmail.com>
2025-03-26 15:30:35 +0100

    Added locale files for spanish and italian,

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -3,8 +3,20 @@ import { init, register } from "svelte-i18n";
 
 const defaultLocale = "de";
 
-register("en", () => import("./en.json"));
-register("de", () => import("./de.json"));
+const localeFileCollection: { languageKey: string, path: string }[] = [
+    { languageKey: "en", path: "./en.json" },
+    { languageKey: "de", path: "./de.json" },
+    { languageKey: "jp", path: "./jp.json" },
+    { languageKey: "es", path: "./es.json" },
+    { languageKey: "it", path: "./it.json" }
+]
+
+localeFileCollection.forEach((collection) => {
+    register(
+        collection.languageKey, 
+        () => import(collection.path)
+    );
+});
 
 init({
     fallbackLocale: defaultLocale,
